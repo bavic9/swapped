a676ec6ee8fedc6d80fad710e8f2aac2368af9f8
Bavic <bavic97@gmail.com>
2024-03-16 15:48:01 +0100

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '979px',
+      xl: '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+
